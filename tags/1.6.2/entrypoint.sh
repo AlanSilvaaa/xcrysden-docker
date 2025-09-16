0be969e5e6d5fa0b8e48355fa4afc4bf0fd2a120
AlanSilvaaa <asilvar4@correo.uss.cl>
2025-09-16 00:39:52 -0300

    remove 1.6.2 tag

--- tags/1.6.2/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# If the first argument is "--convert", run the Python script
-if [[ "$1" == "--convert" ]]; then
-    shift
-    python3 /app/convert_cif_to_xsf.py "$@"
-else
-    exec ./xcrysden
-fi
